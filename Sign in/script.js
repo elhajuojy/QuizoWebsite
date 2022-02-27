f69d360ab1c1c2df68b1e411799663b1b1f3fe28
elhajuojy <souirimehdi311@gmail.com>
2022-02-27 12:29:28 +0100

    add some update

--- Sign in/script.js
+++ Sign in/script.js
@@ -1,17 +1,26 @@
 const nameInput = document.querySelector('input[name="userNameOrEmail"]');
 const passwordInput = document.querySelector('input[name="password"]');
 const error_hint = document.querySelector('.error-hint');
+const error_hintpass = document.querySelector('.error_hintpass');
 const btn = document.querySelector(".btn-signIn");
+
 
 
 const validateInputs = () => {
     nameInput.classList.remove("invalid");
-    passwordInput.classList.remove('invalid');
     error_hint.classList.add("hidden");
-    if (!nameInput.value || !passwordInput.value) {
-        nameInput.classList.add("invalid");
-        passwordInput.classList.add('invalid');
+    if (!nameInput.value) {
+        nameInput.classList.add('invalid');
         error_hint.classList.remove("hidden");
+    }
+};
+
+const validateInputsPass = () => {
+    passwordInput.classList.remove('invalid');
+    error_hintpass.classList.add("hidden");
+    if (!passwordInput.value) {
+        passwordInput.classList.add("invalid");
+        error_hintpass.classList.remove("hidden");
     }
 };
 
@@ -24,4 +33,7 @@ btn.addEventListener("click", (e) => {
 
 nameInput.addEventListener("input", () => {
     validateInputs();
+})
+passwordInput.addEventListener("input", () => {
+    validateInputsPass();
 })
