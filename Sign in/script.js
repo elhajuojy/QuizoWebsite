0caea82eccf4912b6eb91db463f698c9e4585c94
elhajuojy <souirimehdi311@gmail.com>
2022-02-27 21:24:38 +0100

    some update

--- Sign in/script.js
+++ Sign in/script.js
@@ -5,8 +5,25 @@ const error_hintpass = document.querySelector('.error_hintpass');
 const btn = document.querySelector(".btn-signIn");
 const url = 'SignInData.json';
 
+btn.onclick = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+        console.log(xhr.responseText);
+        // const data = JSON.parse(xhr.responseText);
+        // outputData(data);
+    }
+    xhr.open('GET', url);
+    xhr.send();
+};
 
-
+// function outputData(vals) {
+//     main.innerHTML = "";
+//     vals.forEach((ele, ind) => {
+//         console.log(ele.first, ele.last);
+//         const div = document.createElement('div');
+//         main.append(div);
+//         div.innerHTML = `${ele.first} ${ele.last}`;
+//     });
 
 
 const validateInputs = () => {
@@ -42,3 +59,4 @@ nameInput.addEventListener("input", () => {
 passwordInput.addEventListener("input", () => {
     validateInputsPass();
 })
+
