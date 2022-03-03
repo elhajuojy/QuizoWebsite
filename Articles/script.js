b7344aa12c44e8a71b06e961363074fd1e425b50
elhajuojy <souirimehdi311@gmail.com>
2022-03-03 23:05:58 +0100

    add some arcticle page code

--- Articles/script.js
+++ Articles/script.js
@@ -1,12 +1,12 @@
-const explore = document.querySelector('.explore-more');
-const art = document.querySelector('art1');
 
-function exploreonclik() {
-    art.classList.remove('hidden');
 
-}
+
 
 function menuToggle() {
     const toggleMenu = document.querySelector('.menu');
     toggleMenu.classList.toggle('active');
+
 }
+
+
+
